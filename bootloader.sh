fbe61bce2734c22b09e6bb4693d244c02072db9a
Gowtham Alaguraj <gowtham25alaguraj@gmail.com>
2023-01-09 16:27:51 +0000

    Added file for scalingo deployment

---
+++ bootloader.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function download_latest_release() {
+  eval "curl -s https://api.github.com/repos/$1/releases/latest | grep browser_download_url | grep $2 | cut -d : -f 2,3 | tr -d \\\" | wget -qi -"
+}
+
+OS_ARCHITECTURE="386"
+
+ARTIFACT_NAME="cloud-torrent_linux_${OS_ARCHITECTURE}"
+
+echo "Downloading artifact, $ARTIFACT_NAME.gz"
+download_latest_release jpillora/cloud-torrent "${OS_ARCHITECTURE}"
+echo "Artifact $ARTIFACT_NAME.gz downloaded successfully"
+
+echo "Unzipping Artifact $ARTIFACT_NAME.gz"
+gunzip "$ARTIFACT_NAME.gz"
+echo "Artifact $ARTIFACT_NAME.gz unzipped successfully"
+
+chmod +x $ARTIFACT_NAME
+mv "$ARTIFACT_NAME" cloud-torrent
+
+echo "Starting up cloud-torrent..."
+
+export PATH="$(pwd):${PATH}"
+chmod +x startCloudTorrent.sh && ./startCloudTorrent.sh
